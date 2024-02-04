6a1b77c7e967f6b6ef573b9b936c5a838a2fe95b
pmachio <machio3k@gmail.com>
2024-02-04 18:46:49 +0100

    Correción script para no depender del nombre del contenedor

--- TrabajoFinDeMaster/script_5_stream.sh
+++ TrabajoFinDeMaster/script_5_stream.sh
@@ -4,4 +4,4 @@
 
 #Enviamos al cluster el driver de spark stream
 #Notas que pasamos como paquete spark-sql-kafka que debe concindir co las version de spark que tiene el contenedor
-docker exec -it entorno_spark-worker_1 /spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2 --class org.uam.masterbigdata.drivers.StreamDriver --deploy-mode client --master spark://spark-master:7077 --verbose --supervise /opt/spark-apps/spark_project.jar
+docker exec -it $(docker-compose -f entorno/docker-compose.yml ps -q spark-worker) /spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2 --class org.uam.masterbigdata.drivers.StreamDriver --deploy-mode client --master spark://spark-master:7077  --supervise /opt/spark-apps/spark_project.jar
--- TrabajoFinDeMaster/script_6_stream_producer.sh
+++ TrabajoFinDeMaster/script_6_stream_producer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #Usando el contenedor de kafka. Lanzamos el script para crear el topic "telemetry" y arracamos un productor en consola para cargar los datos.
-docker exec -it entorno_kafka_1 bash /opt/kafka_2.13-2.8.1/script/create_topic.sh
+cd entorno/
+docker exec -it $(docker-compose ps -q kafka) bash /opt/kafka_2.13-2.8.1/script/create_topic.sh
 
